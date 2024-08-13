f5b95d3be53ef2339f17339786fb8e7eb070865f
Purwanto . <kontakmaspoe@gmail.com>
2024-08-13 02:56:37 +0000

    Update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x9d27790990CA8Ecbebc6eFc5F447fDEcfC326783'
+export const ProxyAdmin = '0x39dD41e9A49cFE53Af96354DFAfe8E87a3bA7dAc'
+export const SWTRImplementation = '0xE64375BBCE4a97d9F1b5b23f81Cf47060Ec64BF9'
     
